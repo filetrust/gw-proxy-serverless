68c7572edefda47a25448f3d9bfb67d4d9ad50a9
Oleg Taranenko MP <olegtaranenko@gmail.com>
2020-03-04 21:08:46 +0100

    complete first draft of the wireframe 6

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 import ReactSpeedometer from 'react-d3-speedometer';
 
-import BarChart from './components/barChart';
-import KeySuccess from './components/keySuccess';
-import ProjectTable from './components/projectTable';
-import Note from './components/note';
+import KeySuccess  from './components/keySuccess';
+import MetricTable from './components/metricTable';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'semantic-ui-css/semantic.min.css';
 
 import { Row, Col, Container } from 'reactstrap';
-import { Card, Icon } from 'semantic-ui-react';
+import { Card } from 'semantic-ui-react';
 
 function App() {
   return (
@@ -19,44 +16,27 @@ function App() {
       <Container>
         <Row className="my-5">
           <div className="title">Financials</div>
-          <div className="ml-auto" style={{ height: 200 }}>
-            <ReactSpeedometer value={400} height={120} width={240}/>
+          <div className="ml-auto" style={{ height: 120 }}>
+            <ReactSpeedometer value={200} height={120} width={240}/>
           </div>
         </Row>
 
         <Row className="mt-4">
           <Col md={6}>
-            <Card fluid>
-            </Card>
-            <KeySuccess />
+            <div className="d-flex flex-column justify-content-between" style={{height: "100%"}}>
+              <div className="subtitle">Headlines</div>
+              <div className="subtitle mb-4">Financial Summary</div>
+            </div>
           </Col>
           <Col md={6}>
-            <Card fluid>
-              <Card.Content header="Top 10 Projects" />
-              <Card.Content>
-                <ProjectTable />
-              </Card.Content>
-            </Card>
-            <div className="top-ten-projects-footer">
-              <div className="top-ten-projects-label">Top Ten Subtotal</div>
-              <div className="top-ten-projects-value"></div>
-            </div>
-            <div className="top-ten-projects-footer">
-              <div className="top-ten-projects-label">All Clients Total</div>
-              <div className="top-ten-projects-value"></div>
-            </div>
+            <div className="subtitle">Key Metrics</div>
+            <MetricTable />
+            <KeySuccess />
           </Col>
         </Row>
-        <Row>
-          <Col md={4}>
-            <Note title="Issues To Be Addressed" />
-          </Col>
-          <Col md={3}>
-            <Note title="Finances" asc />
-          </Col>
-          <Col md={5}>
-            <Note title="New Target Clients + Actions To Pursue" />
-          </Col>
+        <Row md={12}>
+          <Card fluid style={{height: 500}}>
+          </Card>
         </Row>
       </Container>
     </div>
--- client/src/components/keySuccess/index.js
+++ client/src/components/keySuccess/index.js
@@ -3,7 +3,7 @@ import './style.scss';
 export default function KeySuccess() {
   return (
     <div className="key-success">
-      <div className="key-success-header">KEY SUCCESS THIS MONTH</div>
+      <div className="key-success-header">KEY SUCCESS</div>
       <div className="key-success-content"></div>
     </div>
   );
