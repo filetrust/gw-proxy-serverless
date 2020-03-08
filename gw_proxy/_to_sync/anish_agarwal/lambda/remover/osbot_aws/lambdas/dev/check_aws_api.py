25c32d50670fc5bc0e4d3c3dda0af82a87fbfd1d
Abhi <abhi.coderx@gmail.com>
2020-03-08 16:12:03 +0530

    Removed bot modules to easy review

--- gw_proxy/_to_sync/anish_agarwal/lambda/remover/osbot_aws/lambdas/dev/check_aws_api.py
+++
@@ -1,4 +0,0 @@
-
-def run(event, context):
-    from osbot_aws.Globals import Globals
-    return 'checking aws api: {0}'.format(Globals)
--- gw_proxy/_to_sync/anish_agarwal/lambda/remover/osbot_aws/lambdas/dev/hello_world.py
+++
@@ -1,2 +0,0 @@
-def run(event, context):
-    return 'From lambda code, hello {0}'.format(event.get('name'))
